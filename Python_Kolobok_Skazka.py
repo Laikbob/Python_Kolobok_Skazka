40087ccbbc3cf98f17d1d98555e91196dcfb83f8
Andrei Lebedev <andreilebedev321@gmail.com>
2025-01-08 15:31:25 +0200

    l

--- Python_Kolobok_Skazka.py
+++ Python_Kolobok_Skazka.py
@@ -1,3 +1,4 @@
+#Kolobok
 heroes = ["Заяц", "Волк", "Медведь", "Лиса"]
 
 for hero in heroes:
@@ -8,3 +9,27 @@ for hero in heroes:
     else:
         print("Колобок: Я от тебя уйду!")
 
+#Nädala päevad
+def get_weekday(day):
+    days = {
+        1: "Esmaspäev (Понедельник)",
+        2: "Teisipäev (Вторник)",
+        3: "Kolmapäev (Среда)",
+        4: "Neljapäev (Четверг)",
+        5: "Reede (Пятница)",
+        6: "Laupäev (Суббота)",
+        7: "Pühapäev (Воскресенье)"
+    }
+    return days.get(day, "Неверный номер дня недели")
+
+
+while True:
+    print("Введите номер дня недели (1-7) или 0 для выхода.")
+    try:
+        day_number = int(input("Ваш выбор: "))
+        if day_number == 0:
+            print("Программа завершена.")
+            break
+        print(get_weekday(day_number))
+    except ValueError:
+        print("Пожалуйста, введите корректное число.")
